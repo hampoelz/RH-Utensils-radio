f27b943e601f9c2c226ab6ada8612f76599eaa48
Rene Hampölz <service@hampoelz.net>
2019-07-02 17:40:38 +0200

    Update App.xaml.cs

--- Radio.Wpf/App.xaml.cs
+++ Radio.Wpf/App.xaml.cs
@@ -1,9 +1,60 @@
-﻿using System.Windows;
+﻿using MaterialDesignThemes.Wpf;
+using Radio.Wpf.Utilities;
+using Sample_NAudio;
+using System.IO;
+using System.Windows;
 
 namespace Radio.Wpf
 {
     public partial class App : Application
     {
-        public static string File;
+        public static string Title;
+
+        private static string file;
+
+        public static string File
+        {
+            get => file;
+            set
+            {
+                if (value.EndsWith(".radio"))
+                {
+                    Pages.Player.FileName.Text = Path.GetFileNameWithoutExtension(value);
+
+                    var content = System.IO.File.ReadAllText(value);
+
+                    (string title, string url) = Converter.Online.Youtube(content);
+
+                    if (title == null || url == null)
+                    {
+                        value = content;
+                        Title = null;
+                    }
+                    else
+                    {
+                        value = url;
+                        Title = title;
+                    }
+                }
+                else
+                {
+                    Title = null;
+                }
+
+                file = value;
+
+                if (string.IsNullOrEmpty(file)) return;
+
+                AudioPlayer.Instance.OpenFile(file);
+
+                if (AudioPlayer.Instance.CanPlay)
+                {
+                    AudioPlayer.Instance.Play();
+                    Pages.Player.PausePlayIcon.Kind = PackIconKind.PauseCircleOutline;
+
+                    Pages.Player.StopButton.IsEnabled = true;
+                }
+            }
+        }
     }
 }
